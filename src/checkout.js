c23cdda66608e6c28dcf5469c2ba601430bb54c6
gabriel <gabrielratz84@gmai.com>
2021-02-26 06:42:11 +0100

    checkout

--- src/checkout.js
+++ src/checkout.js
@@ -8,6 +8,28 @@ import addToCartDOM from "./addToCartDOM.js";
 let cart = getStorageItem("cart");
 let store = getStorageItem("store");
 
+function test(element) {
+  const rightCheckout = getElement(".right-checkout");
+  const totalBtn = getElement(".total-btn");
+  const item = document.createElement("div");
+  item.classList.add("single-item-checkout");
+
+  item.innerHTML = `
+    <h3>${element.title}</h3>
+    <p>${formatPrice(element.price)}</p>
+    <p>Amount: ${element.amount}</p>
+  `;
+
+  rightCheckout.appendChild(item);
+
+  const totalAmount = getElement(".total-amount");
+  let total = cart.reduce((total, cartItem) => {
+    return (total += cartItem.price * cartItem.amount);
+  }, 0);
+  totalAmount.textContent = `Total : ${formatPrice(total)} `;
+}
+
 cart.forEach((element) => {
-  addToCartDOM(element);
+  console.log("ele", element);
+  test(element);
 });
