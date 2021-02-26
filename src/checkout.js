2e09cf0507090ff6d007de7f7c7ec8aa279e0870
gabriel <gabrielratz84@gmai.com>
2021-02-26 05:55:19 +0100

    checkout

---
+++ src/checkout.js
@@ -0,0 +1,13 @@
+import { getElement, formatPrice, getStorageItem } from "./utils.js";
+import data from "./items.js";
+import displayItems from "./displayItems.js";
+import setupStore from "./setupStore.js";
+import "./toggle.js";
+import addToCartDOM from "./addToCartDOM.js";
+
+let cart = getStorageItem("cart");
+let store = getStorageItem("store");
+
+cart.forEach((element) => {
+  addToCartDOM(element);
+});
